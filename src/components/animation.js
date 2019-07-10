8ad9efea582c8180970e66b854f0ea16cc0391f3
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-07-09 23:47:21 -0300

    Add some basic video mappings

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,48 +1,61 @@
 import React from 'react';
 import presets from 'utils/presets';
+import intro from '../../assets/video/intro.mp4';
 
 const isSetsEqual = (a, b) => a.size === b.size && [...a].every(value => b.has(value));
 
 const Animation = ({
   texts,
 }) => {
+  const video = React.useRef(null);
   const [rendering, setRendering] = React.useState([]);
 
   React.useEffect(() => {
-    const sequence = presets.sequence.map((s, i) => {
-      s = {
-        ...presets.defaults,
-        ...s,
-        end: s.start + s.duration,
-        id: i,
-      };
+    const start = () => {
+      video.current.play();
+      const sequence = presets.sequence.map((s, i) => {
+        s = {
+          ...presets.defaults,
+          ...s,
+          id: i,
+        };
 
-      return {
-        ...s,
-        texts: (texts[i] || []).map((t, i) => ({
-          className: s.classes[i] || '',
-          text: t,
-        })),
-      };
-    });
+        if (s.end) {
+          s.duration = s.end - s.start;
+        } else if (s.duration) {
+          s.end = s.start + s.duration;
+        }
 
-    const start = new Date();
-    const getElapsed = () => new Date() - start;
 
-    setInterval(() => {
-      const elapsed = getElapsed() / 1000;
-      const renderingIds = new Set(rendering.map(s => s.id));
-      const shouldRender = sequence.filter(s => s.start <= elapsed && s.end >= elapsed);
-      const shouldRenderIds = new Set(shouldRender.map(s => s.id));
-      const changed = !isSetsEqual(renderingIds, shouldRenderIds);
-      if (changed) {
-        setRendering(shouldRender);
-      }
-    }, 50);
+        return {
+          ...s,
+          texts: (texts[i] || []).map((t, i) => ({
+            className: s.classes[i] || '',
+            text: t,
+          })),
+        };
+      });
+
+      const start = new Date();
+      const getElapsed = () => new Date() - start;
+
+      setInterval(() => {
+        const elapsed = getElapsed() / 1000;
+        const renderingIds = new Set(rendering.map(s => s.id));
+        const shouldRender = sequence.filter(s => s.start <= elapsed && s.end >= elapsed);
+        const shouldRenderIds = new Set(shouldRender.map(s => s.id));
+        const changed = !isSetsEqual(renderingIds, shouldRenderIds);
+        if (changed) {
+          setRendering(shouldRender);
+        }
+      }, 50);
+    };
+    video.current.addEventListener('canplaythrough', start);
   }, []);
 
   return (
     <>
+      <video muted ref={video} className="intro" src={intro} />
       {rendering.map(r => (
         <div
           key={r.id}
